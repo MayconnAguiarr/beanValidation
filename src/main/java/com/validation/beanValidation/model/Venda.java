c80c443b8369b831266946c29752b24dbc969961
Maycon <mayconn.aguiarr@gmail.com>
2018-02-11 22:03:17 -0200

    adicionado uma anotação para validar o Total da venda caso o tipo seja BRINDE ou PADRÃO

--- src/main/java/com/validation/beanValidation/model/Venda.java
+++ src/main/java/com/validation/beanValidation/model/Venda.java
@@ -8,6 +8,9 @@ import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.validation.beanValidation.validation.TotalPorTipo;
+
+@TotalPorTipo
 public class Venda {
 	
 	@NotNull
